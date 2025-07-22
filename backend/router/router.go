07be89dd411ba7c9bb56f7cde01faa282db454fb
Huy Le <hle2@gohealth.com>
2025-07-22 14:09:30 +0700

    add cors for router

--- backend/router/router.go
+++ backend/router/router.go
@@ -6,12 +6,23 @@ import (
 	"backend/internal/middleware"
 	"backend/internal/repository"
 	"backend/internal/service"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // domain FE của bạn
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	bitbucketCfg := config.NewBitbucketConfig()
 	bitbucketRepo := repository.NewBitbucketRepo(bitbucketCfg)
